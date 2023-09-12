c7b9432dd19c61dec79ec56190acd74d4986a7ce
aaaa <dilshodovziyodillo@gmail.com>
2023-09-12 22:28:12 +0500

    clincode  commit-2

--- Shamsheer.Domain/Entities/User.cs
+++ Shamsheer.Domain/Entities/User.cs
@@ -1,19 +1,19 @@
-﻿using Shamsheer.Domain.Commons;
-using Shamsheer.Domain.Enums;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Shamsheer.Domain.Enums;
+using Shamsheer.Domain.Commons;
+using System.Collections.Generic;
 
-namespace Shamsheer.Domain.Entities
+
+namespace Shamsheer.Domain.Entities;
+
+public class User : Auditable
 {
-    public class User : Auditable
-    {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Phone { get; set; }
-        public string Password { get; set; }
-        public UserRole Role { get; set; }
-    }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Phone { get; set; }
+    public string Password { get; set; }
+    public UserRole Role { get; set; }
 }
