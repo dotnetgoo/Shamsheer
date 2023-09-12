21d4c3ecf336cb4c6f7bcfe79f8fe2a99ca5f403
Muhammadkarim To'xtaboyev <toxtaboyev.m@icloud.com>
2023-09-12 16:25:56 +0500

    Auditable created

---
+++ Shamsheer.Domain/Commons/Auditable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shamsheer.Domain.Commons
+{
+    public class Auditable 
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
