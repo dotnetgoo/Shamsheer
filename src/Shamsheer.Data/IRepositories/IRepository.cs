9e028e5c1ea4a6bcae998b06d41d158184c05204
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-10-06 21:36:46 +0500

    Repositories added to Project

---
+++ src/Shamsheer.Data/IRepositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shamsheer.Data.IRepositories;
+
+public interface IRepository<TEntitiy>
+{
+    public Task<bool> RemoveAsync(long id);
+    public IQueryable<TEntitiy> SelectAllAsync();
+    public Task<TEntitiy> SelectByIdAsync(long id);
+    public Task<TEntitiy> InsertAsync(TEntitiy entitiy);
+    public Task<TEntitiy> UpdateAsync(TEntitiy entitiy);
+}
---
+++ src/Shamsheer.Data/Repositories/Repository.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Shamsheer.Domain.Commons;
+using Shamsheer.Data.DbContexts;
+using Shamsheer.Data.IRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Shamsheer.Data.Repositories;
+
+public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
+{
+    ShamsheerDbContext dbContext;
+    DbSet<TEntity> dbSet;
+
+    public Repository()
+    {
+        dbContext = new ShamsheerDbContext();
+        this.dbSet = dbContext.Set<TEntity>();
+    }
+
+    public async Task<TEntity> InsertAsync(TEntity entitiy)
+    {
+        await dbSet.AddAsync(entitiy);
+        await dbContext.SaveChangesAsync();
+        return entitiy;
+    }
+
+    public async Task<bool> RemoveAsync(long id)
+    {
+        var entity = await dbSet.FirstOrDefaultAsync(e => e.Id == id);
+        dbSet.Remove(entity);
+        await dbContext.SaveChangesAsync();
+        return true;
+    }
+
+    public IQueryable<TEntity> SelectAllAsync()
+        => this.dbSet;
+
+    public async Task<TEntity> SelectByIdAsync(long id)
+        => await dbSet.FirstOrDefaultAsync(e => e.Id == id);
+
+    public async Task<TEntity> UpdateAsync(TEntity entitiy)
+    {
+        var entities = (dbContext.Update(entitiy)).Entity;
+        await dbContext.SaveChangesAsync();
+        return entities;
+    }
+}
+
