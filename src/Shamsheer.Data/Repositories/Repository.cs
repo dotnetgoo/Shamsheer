2e1e0a3b81f6fbb444b73bfc6849b4249ec3340f
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-10-13 15:51:28 +0500

    Repsotpry corrected in project

--- src/Shamsheer.Data/Repositories/Repository.cs
+++ src/Shamsheer.Data/Repositories/Repository.cs
@@ -9,33 +9,33 @@ namespace Shamsheer.Data.Repositories;
 
 public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditable
 {
-    private readonly ShamsheerDbContext dbContext;
+    private readonly ShamsheerDbContext _dbContext;
     private readonly DbSet<TEntity> dbSet;
 
-    public Repository()
+    public Repository(ShamsheerDbContext dbContext)
     {
-        dbContext = new ShamsheerDbContext();
-        this.dbSet = dbContext.Set<TEntity>();
+        this._dbContext = dbContext;
+        this.dbSet = _dbContext.Set<TEntity>();
     }
 
     
     public async Task<TEntity> InsertAsync(TEntity entity)
     {
-        await dbSet.AddAsync(entity);
+        await this.dbSet.AddAsync(entity);
         return entity;
     }
 
 
     public async Task<bool> RemoveAsync(long id)
     {
-        var entity = await dbSet.FirstOrDefaultAsync(e => e.Id == id);
+        var entity = await this.dbSet.FirstOrDefaultAsync(e => e.Id == id);
         dbSet.Remove(entity);
         return true;
     }
 
 
     public async Task<bool> SaveChangeAsync()
-        => await this.dbContext.SaveChangesAsync() > 0;
+        => await this._dbContext.SaveChangesAsync() > 0;
 
 
     public IQueryable<TEntity> SelectAll()
@@ -50,7 +50,7 @@ public class Repository<TEntity> : IRepository<TEntity> where TEntity : Auditabl
 
     public async Task<TEntity> UpdateAsync(TEntity entity)
     {
-        var entities = (dbContext.Update(entity)).Entity;
+        var entities = (this._dbContext.Update(entity)).Entity;
         return entities;
     }
 }
