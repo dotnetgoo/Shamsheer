111669951061b17303ec93e12dd061a277f47ba8
Mukhammadkarim Tukhtaboev <75162831+dotnetgoo@users.noreply.github.com>
2023-10-13 18:55:13 +0500

    UserPasswordMigration created

---
+++ src/Shamsheer.Data/Migrations/20231013135436_PasswordDeletedFromUserMigration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Shamsheer.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class PasswordDeletedFromUserMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
