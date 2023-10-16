4d5b277c993cd05a1d57198062eb8e89bc063e2b
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-10-16 16:58:40 +0500

    Middlewares, Models, Exceptions added to MessengerApi

---
+++ src/Shamsheer.Messenger.Api/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Shamsheer.Messenger.Api.Models
+{
+    public class Response
+    {
+        public int Code { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
