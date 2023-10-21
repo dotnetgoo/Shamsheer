f0c21028d1d569975227af48d71802477ae55853
DilafruzaJoraboyeva <dilafruzjraboyeva@gmail.com>
2023-10-21 23:30:54 +0500

    Group.Service.Update is fixed and '-' is added to private variables.

--- src/Shamsheer.Service/Services/Groups/GroupService.cs
+++ src/Shamsheer.Service/Services/Groups/GroupService.cs
@@ -1,13 +1,13 @@
-﻿using AutoMapper;
-using Microsoft.EntityFrameworkCore;
-using Shamsheer.Data.IRepositories;
-using System;
+﻿using System;
+using AutoMapper;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-using Shamsheer.Service.Exceptions;
 using Shamsheer.Domain.Enums.Chats;
+using Shamsheer.Service.Exceptions;
+using Shamsheer.Data.IRepositories;
 using Shamsheer.Service.DTOs.Groups;
+using Microsoft.EntityFrameworkCore;
 using Shamsheer.Domain.Entities.Chats;
 using Shamsheer.Service.Interfaces.Groups;
 
@@ -15,100 +15,84 @@ namespace Shamsheer.Service.Services.Groups;
 
 public class GroupService : IGroupService
 {
-    private readonly IMapper mapper;
-    private readonly IUserRepository userRepository;
-    private readonly IGroupRepository groupRepository;
+    private readonly IMapper _mapper;
+    private readonly IUserRepository _userRepository;
+    private readonly IGroupRepository _groupRepository;
 
     public GroupService(
         IMapper mapper,
         IUserRepository userRepository,
         IGroupRepository groupRepository)
     {
-        this.mapper = mapper;
-        this.userRepository = userRepository;
-        this.groupRepository = groupRepository;
+        this._mapper = mapper;
+        this._userRepository = userRepository;
+        this._groupRepository = groupRepository;
     }
-    public async Task<GroupForResultDto> CreateAsync(GroupForCreationDto dto)
+    public async Task<GroupForResultDto> AddAsync(GroupForCreationDto dto)
     {
 
         // After implementing PermissionServices
-        var user = await this.userRepository.SelectAll()
+        var user = await _userRepository.SelectAll()
             .Where(u => u.Id == dto.OwnerId)
             .FirstOrDefaultAsync();
         if (user is null)
-            throw new ShamsheerException(404, "Owner is not found");
+            throw new ShamsheerException(404, "Owner is not found.");
 
-        var mappedGroup = this.mapper.Map<Group>(dto);
+        var mappedGroup = _mapper.Map<Group>(dto);
         mappedGroup.ChatType = ChatType.Group;
         mappedGroup.InviteLink = "";
         mappedGroup.CreatedAt = DateTime.UtcNow;
 
-        var result = await this.groupRepository.InsertAsync(mappedGroup);
+        var result = await _groupRepository.InsertAsync(mappedGroup);
 
-        return this.mapper.Map<GroupForResultDto>(result);
+        return _mapper.Map<GroupForResultDto>(result);
     }
 
     public async Task<GroupForResultDto> ModifyAsync(long id, GroupForUpdateDto dto)
     {
-        var group = await this.groupRepository.SelectAll()
+        var group = await _groupRepository.SelectAll()
             .Where(g => g.Id == id)
             .FirstOrDefaultAsync();
         if (group is null)
-            throw new ShamsheerException(404, "Group is not found");
+            throw new ShamsheerException(404, "Group is not found.");
 
-        var mappedGroup = this.mapper.Map(dto, group);
+        var mappedGroup = _mapper.Map(dto, group);
         mappedGroup.UpdatedAt = DateTime.UtcNow;
 
-        await this.groupRepository.UpdateAsync(mappedGroup);
+        await _groupRepository.UpdateAsync(mappedGroup);
 
-        return this.mapper.Map<GroupForResultDto>(mappedGroup);
-    }
-
-    public Task<GroupForResultDto> ModifyDescriptionAsync(string description)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<GroupForResultDto> ModifyTitleAsync(string title)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Task<GroupForResultDto> ModifyUsernameAsync(string username)
-    {
-        throw new NotImplementedException();
+        return _mapper.Map<GroupForResultDto>(mappedGroup);
     }
 
     public async Task<bool> RemoveAsync(long id)
     {
-        var group = await this.groupRepository.SelectAll()
+        var group = await _groupRepository.SelectAll()
             .Where (g => g.Id == id)
             .FirstOrDefaultAsync();
         if (group is null)
-            throw new ShamsheerException(404, "Group is not found");
+            throw new ShamsheerException(404, "Group is not found.");
 
-        await this.groupRepository.DeleteAsync(id);
+        await _groupRepository.DeleteAsync(id);
         return true;
     }
 
     public async Task<IEnumerable<GroupForResultDto>> RetrieveAllAsync()
     {
         //   Do pagination logic
-        var groups =  this.groupRepository.SelectAll().AsNoTracking();
+        var groups =  _groupRepository.SelectAll().AsNoTracking();
 
-         return this.mapper.Map<IEnumerable<GroupForResultDto>>(groups);
-            
+         return _mapper.Map<IEnumerable<GroupForResultDto>>(groups);    
     }
 
     public async Task<GroupForResultDto> RetrieveByIdAsync(long id)
     {
-        var group = await this.groupRepository.SelectAll()
+        var group = await _groupRepository.SelectAll()
             .Where (g => g.Id == id)
             .FirstOrDefaultAsync();
         if (group is null)
             throw new ShamsheerException(404, "Group is not found");
 
-        var mappedGroup = this.mapper.Map<GroupForResultDto>(group);
+        var mappedGroup = _mapper.Map<GroupForResultDto>(group);
 
         return mappedGroup;
     }
