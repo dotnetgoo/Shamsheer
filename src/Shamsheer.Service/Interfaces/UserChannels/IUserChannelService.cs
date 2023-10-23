9290e2047265ec291f0600b1bb7d58decb0ea71f
Bakhtiyorovna <kralecabakhtiyorovna@gmail.com>
2023-10-23 23:45:11 +0500

    Removed public access modifier from interface methods

--- src/Shamsheer.Service/Interfaces/UserChannels/IUserChannelService.cs
+++ src/Shamsheer.Service/Interfaces/UserChannels/IUserChannelService.cs
@@ -5,9 +5,9 @@ using Shamsheer.Service.DTOs.UserChannels;
 namespace Shamsheer.Service.Interfaces.UserChannel;
 public interface IUserChannelService
 {
-    public Task<bool> RemoveAsync(long id);
-    public Task<UserChannelForResultDto> RetrieveByIdAsync(long id);
-    public Task<IEnumerable<UserChannelForResultDto>> RetrieveAllAsync();
-    public Task<UserChannelForResultDto> CreateAsync(UserChannelForCreationDto dto);
-    public Task<UserChannelForResultDto> ModifyAsync(long id, UserChannelForUpdateDto dto);
+    Task<bool> RemoveAsync(long id);
+    Task<UserChannelForResultDto> RetrieveByIdAsync(long id);
+    Task<IEnumerable<UserChannelForResultDto>> RetrieveAllAsync();
+    Task<UserChannelForResultDto> CreateAsync(UserChannelForCreationDto dto);
+    Task<UserChannelForResultDto> ModifyAsync(long id, UserChannelForUpdateDto dto);
 }
