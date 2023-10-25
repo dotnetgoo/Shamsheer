07e2e7d63b69cf342376312d80ab2a5a0a2bb256
Jasurbek Abdunazarov <jasurbekabdunazarov111@gmail.com>
2023-10-25 12:24:56 +0500

    All Registration Pages Added To Messenger.Desktop #12

--- src/Shamsheer.Messenger.Desktop/MainWindow.xaml.cs
+++ src/Shamsheer.Messenger.Desktop/MainWindow.xaml.cs
@@ -1,22 +1,5 @@
-using Shamsheer.Messenger.Desktop.Components;
-using Shamsheer.Messenger.Desktop.Models;
-using Shamsheer.Messenger.Desktop.Pages;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Shamsheer.Messenger.Desktop
 {
@@ -29,48 +12,10 @@ namespace Shamsheer.Messenger.Desktop
         {
             InitializeComponent();
         }
-        private void drag_grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (e.ClickCount == 2)
-            {
-                maximize_btn_Click(sender, null);
-            }
-            else
-            {
-                this.DragMove();
 
-            }
-        }
-
-        private void minimize_btn_Click(object sender, RoutedEventArgs e)
+        private void startMessagingBtn_Click(object sender, RoutedEventArgs e)
         {
-            this.WindowState = WindowState.Minimized;
-        }
-
-        private void maximize_btn_Click(object sender, RoutedEventArgs e)
-        {
-            if (this.WindowState == WindowState.Normal)
-            {
-                windowGrid.Margin = new Thickness(8, 10, 10, 10);
-                WindowStyle = WindowStyle.SingleBorderWindow;
-                WindowState = WindowState.Maximized;
-                WindowStyle = WindowStyle.None;
-            }
-            else
-            {
-                this.WindowState = WindowState.Normal;
-                windowGrid.Margin = new Thickness(0);
-            }
-        }
-
-        private void close_btn_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            mainFrame.Navigate(new Home());
+            MainFrame.NavigationService.Navigate(new Uri("QrCodePage.xaml", UriKind.Relative));
         }
     }
 }
