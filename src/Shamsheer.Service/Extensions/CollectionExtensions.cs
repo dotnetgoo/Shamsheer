df32920d9fada918e56c07071030e3844d0fb25d
Hayotbahrom Nurdinov <hayotbahrom2002@gmail.com>
2023-10-25 21:19:25 +0500

    pagination logic optimized and UserChannelController renamed to UserChannelsController

--- src/Shamsheer.Service/Extensions/CollectionExtensions.cs
+++ src/Shamsheer.Service/Extensions/CollectionExtensions.cs
@@ -21,13 +21,7 @@ namespace Shamsheer.Service.Extensions
                 throw new ArgumentOutOfRangeException(nameof(@params.PageSize), "The page size must be greater than or equal to 1.");
             }
 
-            // Skip the specified number of pages.
-            source = source.Skip((@params.PageIndex - 1) * @params.PageSize);
-
-            // Take the specified number of items from the sequence.
-            source = source.Take(@params.PageSize);
-
-            return source;
+            return source.Take((@params.PageSize * (@params.PageIndex - 1))..(@params.PageSize * (@params.PageIndex - 1) + @params.PageSize));
         }
 
         public static IEnumerable<TEntity> ToPagedList<TEntity>(this IEnumerable<TEntity> source, PaginationParams @params)
@@ -42,13 +36,7 @@ namespace Shamsheer.Service.Extensions
                 throw new ArgumentOutOfRangeException(nameof(@params.PageSize), "The page size must be greater than or equal to 1.");
             }
 
-            // Skip the specified number of pages.
-            source = source.Skip((@params.PageIndex - 1) * @params.PageSize);
-
-            // Take the specified number of items from the sequence.
-            source = source.Take(@params.PageSize);
-
-            return source;
+            return source.Take((@params.PageSize * (@params.PageIndex - 1))..(@params.PageSize * (@params.PageIndex - 1) + @params.PageSize));
         }
 
     }
