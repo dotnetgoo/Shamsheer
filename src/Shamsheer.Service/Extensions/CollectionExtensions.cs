2caa82b58877452585dbf6da4e08c3ed1de75b0b
AbdumajidovAvazbek <abdumajidova51@gmail.com>
2023-11-01 11:34:42 -0700

    #103 Pagination shows data if no size capacity is given

--- src/Shamsheer.Service/Extensions/CollectionExtensions.cs
+++ src/Shamsheer.Service/Extensions/CollectionExtensions.cs
@@ -12,30 +12,25 @@ namespace Shamsheer.Service.Extensions
         public static IQueryable<TEntity> ToPagedList<TEntity>(this IQueryable<TEntity> source, PaginationParams @params)
         {
             if (@params.PageIndex < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(@params.PageIndex), "The page index must be greater than or equal to 1.");
-            }
+                return source.Take(10);
 
-            if (@params.PageSize < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(@params.PageSize), "The page size must be greater than or equal to 1.");
-            }
-
-            return source.Skip(@params.PageSize * (@params.PageIndex - 1)).Take(@params.PageSize);
+            else if (@params.PageSize < 1)
+                return source.Take(@params.PageSize);
+            
+            else 
+                return source.Skip(@params.PageSize * (@params.PageIndex - 1)).Take(@params.PageSize);
         }
 
         public static IEnumerable<TEntity> ToPagedList<TEntity>(this IEnumerable<TEntity> source, PaginationParams @params)
         {
             if (@params.PageIndex < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(@params.PageIndex), "The page index must be greater than or equal to 1.");
-            }
+                return source.Take(10);
 
-            if (@params.PageSize < 1)
-            {
-                throw new ArgumentOutOfRangeException(nameof(@params.PageSize), "The page size must be greater than or equal to 1.");
-            }
-            return source.Skip(@params.PageSize * (@params.PageIndex - 1)).Take(@params.PageSize);
+            else if (@params.PageSize < 1)
+                return source.Take(@params.PageSize);
+
+            else
+                return source.Skip(@params.PageSize * (@params.PageIndex - 1)).Take(@params.PageSize);
         }
 
     }
