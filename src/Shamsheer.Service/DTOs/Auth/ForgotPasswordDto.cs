bbb4f4fe973d9fc914eb8d086e1da8cc52a2d964
Khurshid <xurshidyoldoshev4@gmail.com>
2023-11-11 09:56:49 +0500

    Update ForgotPasswordDto.cs

--- src/Shamsheer.Service/DTOs/Auth/ForgotPasswordDto.cs
+++ src/Shamsheer.Service/DTOs/Auth/ForgotPasswordDto.cs
@@ -5,7 +5,7 @@ namespace Shamsheer.Service.DTOs.Auth;
 public class ForgotPasswordDto
 {
     [Required]
-    [EmailValidation(ErrorMessage = "Invalid email format!")]
-    public string Email { get; set; } = string.Empty;
-    public string NewPassword { get; set; } = string.Empty;
+    [ShEmail(ErrorMessage = "Invalid email format!")]
+    public string Email { get; set; } 
+    public string NewPassword { get; set; }
 }
