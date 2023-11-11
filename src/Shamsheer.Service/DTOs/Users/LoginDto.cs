1733c077e7edffcdee84678cacc8f2883c399265
Khurshid <xurshidyoldoshev4@gmail.com>
2023-11-11 09:56:09 +0500

    Update LoginDto.cs

--- src/Shamsheer.Service/DTOs/Users/LoginDto.cs
+++ src/Shamsheer.Service/DTOs/Users/LoginDto.cs
@@ -5,6 +5,6 @@ namespace Shamsheer.Service.DTOs.Users;
 public class LoginDto
 {
     [Required]
-    [EmailValidation(ErrorMessage = "Invalid email format!")]
+    [ShEmail(ErrorMessage = "Invalid email format!")]
     public string Email { get; set; }
 }
